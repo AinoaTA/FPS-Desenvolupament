71b3365cdb1408d3174377d8b25d49dd48498419
AinoaTA <atabares@edu.tecnocampus.cat>
2021-10-02 00:12:12 +0200

    Assets + Door Trigger

---
+++ Assets/Scripts/DoorTrigger.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorTrigger : MonoBehaviour
+{
+    private Animator door;
+    private void Awake()
+    {
+        door = GetComponent<Animator>();
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+            door.SetBool("Open", true);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+          door.SetBool("Open", false);
+    }
+}
