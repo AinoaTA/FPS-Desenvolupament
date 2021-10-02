48a93dba04f95a926320e3ed0094f2aa85775a7a
AinoaTA <atabares@edu.tecnocampus.cat>
2021-10-02 14:34:52 +0200

    Open door with points

---
+++ Assets/Scripts/DoorS/DoorShoot.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorShoot : MonoBehaviour
+{
+    private Animator animator;
+
+    private void OnEnable()
+    {
+        ShooterPoints.delegateGate += OpenGate;
+    }
+
+    private void OnDisable()
+    {
+        ShooterPoints.delegateGate -= OpenGate;
+    }
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+    public void OpenGate()
+    {
+        animator.SetBool("Open", true);
+    }
+}
---
+++ Assets/Scripts/ShooterPoints.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShooterPoints : MonoBehaviour
+{
+    private int points =0;
+
+    public delegate void DelegateGate();
+    public static DelegateGate delegateGate;
+
+    private void Update()
+    {
+        if (points == 10)
+            delegateGate?.Invoke();
+        if (Input.GetKeyDown(KeyCode.T))
+            points += 2;
+
+
+        print(points);
+    }
+}
+
+
+
